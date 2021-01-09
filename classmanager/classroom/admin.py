585a050cdf3e382bbc30429b5a081c5434f4515d
moh12asad <moh12asad@hotmail.com>
2021-01-09 19:35:26 +0200

    1

---
+++ classmanager/classroom/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User,Student,Teacher,StudentsInClass,category, Contact,MessageToTeacher,ClassNotice ,StudentMarks
+# Register your models here.
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Student)
+admin.site.register(StudentMarks)
+admin.site.register(Teacher)
+admin.site.register(StudentsInClass)
+admin.site.register(category)
+admin.site.register(Contact)
+admin.site.register(ClassNotice)
+admin.site.register(MessageToTeacher)
